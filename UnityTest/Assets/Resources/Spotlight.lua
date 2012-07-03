aca29db53d457064bb523fa5e1a3e644a67e87fd
steve donovan <steve.j.donovan@gmail.com>
2012-07-03 14:16:30 +0200

    Updated UnityTest project

--- UnityTest/Assets/Resources/Spotlight.lua
+++ UnityTest/Assets/Resources/Spotlight.lua
@@ -3,7 +3,7 @@ local unity = require 'Unity'
 return {
     Update = function(self)
         if unity.Input.GetButtonDown 'Jump' then
-            local target = unity.GameObject.Find 'Cube'
+            local target = unity.GameObject.Find 'Sphere'
             self.this.transform:LookAt(target.transform)
         end
     end
